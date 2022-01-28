62b18fbc86a817419de15ca5ad34729f555933d7
Anna <anna.vavrik2002@gmail.com>
2022-01-28 15:53:36 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'Eat', done: true },
+    { description: 'Code', done: false },
+    { description: 'Energy drink', done: false },
+    { description: 'Die', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
